50889afacebfbbca3e4483cf850d08ae1270862b
kouta77 <manuel.salcedo.rd@gmail.com>
2015-09-28 15:44:06 -0400

    Today LAst
    last update from today.

--- ProjectTouchDown/Assets/EnemySpawnScript.cs
+++ ProjectTouchDown/Assets/EnemySpawnScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawnScript : MonoBehaviour {
 	public float MaxX = 5f;
-	public Transform EnemyPrefab;
+
+	public List<Transform> enemyPrefab = new List<Transform>();
+	//public Transform EnemyPrefab;
+
 	public BackgroundController BGC;
 
 
@@ -33,7 +37,14 @@ public class EnemySpawnScript : MonoBehaviour {
 
 	IEnumerator instantiateEnemy(){
 		yield return new WaitForSeconds (Random.Range(1f,2f));
-		var newenemy = Instantiate (EnemyPrefab, new Vector3 (transform.position.x + Random.Range (-MaxX, MaxX), transform.position.y, 0), transform.rotation) as Transform;
+
+		int Max = 0;
+		if(controller.GoalCount > 1)
+			Max = Random.Range(0,enemyPrefab.Count);
+
+		Transform Prefab = enemyPrefab[Random.Range(0,Max)];
+
+		var newenemy = Instantiate (Prefab, new Vector3 (transform.position.x + Random.Range (-MaxX, MaxX), transform.position.y, 0), transform.rotation) as Transform;
 		newenemy.parent = BGC.Lastinastance.transform;
 	}
 
